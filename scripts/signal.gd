e77d764512038fce79c3a0dcc54422e37d6986c8
Kushagra Sharma <kush.kuhu@gmail.com>
2025-10-15 22:10:42 +0530

    Added a beam to indicate a signal and made it rotate randomly

---
+++ scripts/signal.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@onready var raycast: RayCast3D = $RayCast3D
+@onready var mesh: MeshInstance3D = $MeshInstance3D
+
+var t: Tween
+
+func _ready() -> void:
+	_create_tween()
+
+func _create_tween():
+	if t:
+		t.stop()
+	t = get_tree().create_tween()
+	t.tween_property(self, "rotation_degrees", Vector3(randi_range(1, 15), randi_range(1, 15), randi_range(1, 15)), 2)
+	t.tween_callback(_create_tween)
+
+func _process(delta: float) -> void:
+	raycast.force_raycast_update()
+	if raycast.is_colliding():
+		var point := to_local(raycast.get_collision_point())
+		mesh.mesh.height = point.y
+		mesh.position.y = point.y/2
+	
