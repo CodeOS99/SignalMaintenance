74b5aa456159e5bf1ffe8ddd9be198f224e3c5b7
Kushagra Sharma <kush.kuhu@gmail.com>
2025-10-18 11:10:57 +0530

    Added interaction between the moving signal and the satellite receiver.

--- scripts/signal.gd
+++ scripts/signal.gd
@@ -9,22 +9,27 @@ func _ready() -> void:
 	_create_tween()
 
 func _create_tween():
-	if Globals.is_signal:
-		print("signal")
-		return
 	if t:
 		t.stop()
 	t = get_tree().create_tween()
-	t.tween_property(self, "rotation_degrees", Vector3(randi_range(2, 6), randi_range(2, 6), randi_range(2, 6)), 3)
+	t.tween_property(self, "rotation_degrees", Vector3(randi_range(-8, 8), randi_range(-8, 8), randi_range(-8, 8)), 4)
 	t.tween_callback(_create_tween)
 
 func _process(delta: float) -> void:
 	raycast.force_raycast_update()
+	var target_y = -28.3
 	if raycast.is_colliding():
 		var point := to_local(raycast.get_collision_point())
-		mesh.mesh.height = point.y
-		mesh.position.y = point.y/2
+		target_y = point.y
 		var obj := raycast.get_collider()
 		if obj.is_in_group("satellite"):
 			Globals.is_signal = true
-			$MeshInstance3D.material_override
+			var mat = mesh.material_override
+			var green_color = Color(0, 1, 0, .8078)
+			mat.albedo_color = green_color
+			mat.emission_enabled = true
+			mat.emission = green_color
+			mat.emission_energy_multiplier = 10.0
+
+	mesh.mesh.height = target_y
+	mesh.position.y = target_y/2
