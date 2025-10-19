80473b552fce32d708a0a3387f890bdff9d9a889
Kushagra Sharma <kush.kuhu@gmail.com>
2025-10-19 16:07:43 +0530

    Added flashing of buttons randomly

--- scripts/simon_button.gd
+++ scripts/simon_button.gd
@@ -1,4 +1,5 @@
 extends Button
+signal finished_flash
 
 @export var col := "r"
 
@@ -7,12 +8,12 @@ func flash():
 	var t = get_tree().create_tween()
 	var ti = .03
 	t.tween_property(self, "modulate", Color.WHITE, ti)
-	t.tween_property(self, "modulate", Color.DARK_GRAY, ti).set_delay(ti)
+	t.tween_property(self, "modulate", Color(.274, .274, .274), ti).set_delay(ti)
 	t.tween_callback(func():
-		print("finished"))
+		finished_flash.emit())
 
 func _process(delta: float) -> void:
-	disabled = not $"..".can_click
+	disabled = not $"../..".can_click
 
 func _on_pressed() -> void:
-	$"..".user_in += col
+	$"../..".add(col)
--- scripts/simon_says_minigame.gd
+++ scripts/simon_says_minigame.gd
@@ -5,7 +5,13 @@ var user_in := ""
 var ans := ""
 var choices := "rgbv"
 var rounds := 3
-var curr_round := 1
+var curr_round := 0
+@onready var c_btn_map = {
+	"r": $HBoxContainer/RedButton,
+	"g": $HBoxContainer/GreenButton,
+	"b": $HBoxContainer/BlueButton,
+	"v": $HBoxContainer/VioletButton
+}
 
 func make_random(n: int):
 	ans = ""
@@ -13,10 +19,31 @@ func make_random(n: int):
 		ans += choices[randi_range(0, len(choices)-1)]
 
 func next_round():
-	rounds += 1
+	can_click = false
+	curr_round += 1
 	if curr_round > rounds:
-		pass # TODO add win!
+		print("Win")# TODO add win!
 	make_random(curr_round)
+	for c in ans:
+		c_btn_map[c].flash()
+		await c_btn_map[c].finished_flash
+	can_click = true
 
 func start():
-	make_random(1)
+	next_round()
+
+func _process(delta: float) -> void:
+	if visible and curr_round == 0:
+		start()
+
+func add(col):
+	if len(user_in) == len(ans):
+		print("???")
+		return
+	if col != ans[len(user_in)-1]:
+		print("???")
+		return
+	user_in += col
+	if user_in == col:
+		user_in = ""
+		next_round()
